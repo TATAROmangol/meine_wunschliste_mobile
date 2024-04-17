ce79bf50363e09da5d7bc513e903bf0cceb1ae83
TATAROmangol <144809011+TATAROmangol@users.noreply.github.com>
2024-04-17 13:56:41 +0500

    Please enter the commit message for your changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:meine_wunschliste/features/tasks_folders_list/folder_list/folder_list_viev.dart';
 import 'package:meine_wunschliste/features/tasks_folders_list/task_folders_list_viev.dart';
-import 'package:meine_wunschliste/features/tasks_folders_list/tasks_list/task_list.viev.dart';
 
 void main() {
   runApp(TaskFoldersListViev());
